b78fcc470fb81d3c5bb18cc31c1c18b02cb8d54d
Hephaestus144 <collins.storm@gmail.com>
2019-04-25 08:22:41 +0200

    Initial commit

---
+++ src/CommandLine.java
@@ -0,0 +1,10 @@
+import MarketData.Curve;
+
+public class CommandLine 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+}
---
+++ src/MarketData/Curve.java
@@ -0,0 +1,64 @@
+package MarketData;
+
+public class Curve 
+{
+	// properties
+	double[] tenors;
+	double[] rates;
+	
+	
+	// constructors
+	public Curve(double[] tenors, double[] rates)
+	{
+		this.tenors = tenors;
+		this.rates = rates;
+	}
+	
+	
+	// methods
+	public double InterpolateRates(double tenor)
+	{
+		double leftTenor = -1;
+		double leftRate = -1;
+		double rightTenor = -1;
+		double rightRate = -1;
+		
+		for(int i = 0; i < this.tenors.length; i++)
+		{
+			if (tenors[i] < tenor)
+			{
+				leftTenor = tenors[i];
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		if (leftTenor != -1)
+		{
+			for(int i = this.tenors.length - 1; i >= 0; i--)
+			{
+				if(tenors[i] > tenor)
+				{
+					rightTenor = tenors[i];
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			return this.rates[0];
+		}
+		
+		if (rightTenor == -1)
+		{
+			return this.rates[this.rates.length - 1];
+		}
+		
+		return ((rightRate - leftRate)/(rightTenor - leftTenor))*(rightTenor - tenor) + leftRate;
+	}
+}
